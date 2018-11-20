a6dd7c538c9a0dda2c1909ba41c293b3f0b927f0
Bruno Santos <brunosantos@protonmail.com>
2018-11-20 09:20:33 -0300

    add class

--- redblacktree.py
+++ redblacktree.py
@@ -6,3 +6,9 @@ Authors: Bruno Santos <example@example.com>
          Jonathas Silva <jonathas@mail.com>
 
 """
+
+class ClassName(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
