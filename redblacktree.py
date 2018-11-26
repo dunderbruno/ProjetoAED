ce723ca9795b74e0114749d817d1261dd60c9a98
Bruno Santos <brunosantos@protonmail.com>
2018-11-26 07:05:14 -0300

    clean file

--- redblacktree.py
+++ redblacktree.py
@@ -1,25 +1,6 @@
 """
+Red-Black Tree.
 
-Red-Black Tree Module.
-
-Authors: Bruno Santos <example@example.com>
-         Jonathas Silva <jonathas@mail.com>
-
+Authors: Bruno Santos
+         Jonathas Silva
 """
-
-def sucessor(n):
-    """Return sucessor"""
-    return n+1
-
-def nada():
-    """Returna 0"""
-    return 0
-
-class ClassName(object):
-    """docstring for ."""
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
-
-def NOVO_TESTE():
-    pass
