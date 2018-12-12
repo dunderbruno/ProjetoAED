fad3f5df76298887d134102cde3999e8b2691ec0
Bruno Santos <brunosantos@protonmail.com>
2018-12-12 07:31:10 -0300

    esboço do main

---
+++ application.py
@@ -0,0 +1,20 @@
+import redblacktree
+import titulo
+import valida
+
+# ÁRVORE DE TÍTULOS
+#instanciar
+
+# ÁRVORE DE VOTOS
+#instanciar
+
+'''
+O cadastramento de títulos de eleitor possuem as seguintes opções:
+• Cadastrar título;
+• Descadastrar título ;
+• Carregar títulos (VÁRIOS DE UMA VEZ)
+'''
+
+# cadastrar candidatos
+
+# validação
