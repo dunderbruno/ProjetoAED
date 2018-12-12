9baf8de321210a1530589f9f986324857da1c502
Jonathas F. Silva <jonathas_ssilva@hotmail.com>
2018-12-12 08:34:51 -0300

    fix

--- valida.py
+++ valida.py
@@ -5,9 +5,9 @@ def valida(no):
     zon = False
     if len(titulo.getInscricao()) == 8 and titulo.getInscricao().isnumeric():
         ins = True
-    if len(titulo.getSecao()) == 3 and titulo.getSecao().isnumeric():
+    if len(titulo.getSecao()) == 4 and titulo.getSecao().isnumeric():
         sec = True
-    if len(titulo.getZona()) == 4 and titulo.getZona().isnumeric():
+    if len(titulo.getZona()) == 3 and titulo.getZona().isnumeric():
         zon = True
 
     if ins and sec and zon:
