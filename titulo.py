7f0c79dd0a34bf1373b7c7e49241bde35a349f61
Jonathas F. Silva <jonathas_ssilva@hotmail.com>
2018-12-12 07:24:31 -0300

    class titulo

---
+++ titulo.py
@@ -0,0 +1,26 @@
+class Titulo():
+    """Classe Titulo."""
+
+    def __init__(self, inscricao, zona, secao):
+        """Construtor."""
+        self.__inscricao = inscricao
+        self.__zona = zona
+        self.__secao = secao
+
+    def getInscricao():
+        return self.__inscricao
+
+    def setInscricao(self, inscricao):
+        self.__inscricao = inscricao
+
+    def getZona(self):
+        return self.__zona
+
+    def setZona(self, zona):
+        self.__zona = zona
+
+    def getSecao(self):
+        return self.__secao
+
+    def setSecao(self, secao):
+        self.__secao = secao
