e2ed974eddc831a8545d08013e7913336c3ba60a
Bruno Santos <brunosantos@protonmail.com>
2018-12-18 08:26:59 -0300

    ajusta validação individual

--- application.py
+++ application.py
@@ -108,14 +108,16 @@ while True:
         entrada = input('Título de eleitor: ')
         eleitor = titulos.search(entrada)
         if eleitor is not titulos.nil:
-            if valida.valida(eleitor) and (votos.search(entrada) is votos.nil):
+            if valida.valida(eleitor):
                 voto = int(input('Voto: '))
                 titulos.delete(eleitor)
                 votos.insert(eleitor.getData().getNumero())
                 candidatos[voto][1] += 1
                 votos_registrados += 1
+            else:
+                print('Título Inválido!')
         else:
-            print('Título não encontrado')
+            print('Título não encontrado!')
     elif opcao == 3:  # simulação
         os.system('clear')
         while not titulos.isEmpty():
